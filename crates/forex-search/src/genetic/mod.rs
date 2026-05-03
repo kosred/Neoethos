866aa5043c3eafe6ef3227c3f067277177298633
kosred <konstantinoskokkinos1982@gmail.com>
2026-05-03 14:03:28 +0200

    Wire genetic diversity and regime labels modules

--- crates/forex-search/src/genetic/mod.rs
+++ crates/forex-search/src/genetic/mod.rs
@@ -1,12 +1,23 @@
+pub mod diversity;
 pub mod evolution_math;
+pub mod regime_labels;
 pub mod search_engine;
 pub mod smc_indicators;
 pub mod strategy_gene;
 
+pub use diversity::{
+    DiversityArchiveConfig, DiversityKey, EvalMetrics, archive_quality_score, diversity_key,
+    select_diverse_archive, smc_mask,
+};
 pub use evolution_math::{
     EvolutionSearchPolicy, ParentSelectionPolicy, SeenSignatureMemory, SurvivorSelectionPolicy,
     apply_metrics, crossover, gene_signature_hash, generate_random_genes, mutate, new_random_gene,
     score_from_metrics, select_parent_index, select_survivor_indices, unique_candidate_or_retry,
+};
+pub use regime_labels::{
+    RegimeLabelPolicy, RegimeWindow, StrategyRegimeProfile, WindowPerformanceLabel,
+    build_rolling_regime_windows, label_strategies_by_regime_windows, rank_training_profiles,
+    window_quality_score,
 };
 pub use search_engine::{
     evaluate_genes, evolve_search, evolve_search_with_progress,
